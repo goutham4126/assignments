5fabb5a304e5e6070eadd686cced57cefedcc8c7
Goutham <goutham4126@gmail.com>
2026-01-09 11:47:48 +0530

    Images gallery

---
+++ JS-assignments/main.js
@@ -0,0 +1,11 @@
+// document.getElementById("para").style.color="red";
+
+// alert("Hello people");
+
+
+// let s="Goutham";
+// p=s.toLowerCase()
+
+// document.writeln("hello")
+
+prompt("Enter your age:")
