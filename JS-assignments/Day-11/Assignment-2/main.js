e7b40636e89cb08f57d0cbfff9e18a1e7af39695
Goutham <goutham4126@gmail.com>
2026-01-12 17:20:13 +0530

    Tic tac Toe

--- JS-assignments/Day-11/Assignment-2/main.js
+++ JS-assignments/Day-11/Assignment-2/main.js
@@ -1,68 +1,90 @@
+let matrix = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+];
 
-let matrix=[
-    ["","",""],
-    ["","",""],
-    ["","",""]
-]
+function CalculateCount() {
+    let count = 0;
 
-function CalculateCount()
-{
-    let count=0;
-
-    for(let ele of matrix)
-    {
-        for(let x of ele)
-        {
-            if(x==="")
-                count++;
+    for (let row of matrix) {
+        for (let cell of row) {
+            if (cell !== "") count++;
         }
     }
+
     return count;
 }
 
+function DisplayMatrix() {
+    const boxes = [
+        'box-1','box-2','box-3',
+        'box-4','box-5','box-6',
+        'box-7','box-8','box-9'
+    ];
 
-function DisplayMatrix()
-{
+    let values = [];
 
-    const arr1=['box-1','box-2','box-3','box-4','box-5','box-6','box-7','box-8','box-9']
-    const arr2=[]
-    for(let ele of matrix)
-    {
-        for(let x of ele)
-        {
-            arr2.push(x);
-        }        
+    for (let row of matrix) {
+        for (let cell of row) {
+            values.push(cell);
+        }
     }
 
-    for(let i=0;i<arr1.length;i++)
-    {
-        document.getElementById(arr1[i]).innerHTML=arr2[i];
-    }
-}
-
-function Reset()
-{
-    const arr=['box-1','box-2','box-3','box-4','box-5','box-6','box-7','box-8','box-9']
-
-    for(let ele of arr)
-    {
-        document.getElementById(ele).innerHTML="";
+    for (let i = 0; i < boxes.length; i++) {
+        document.getElementById(boxes[i]).innerHTML = values[i];
     }
 }
 
-function TicTacToe(x,y)
-{
+function Reset() {
+    matrix = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ];
 
-    let count=CalculateCount();
-    console.log(count);
-    if(count%2===0)
-    {
-        matrix[x][y]="X";
-        DisplayMatrix();
+    DisplayMatrix();
+}
+
+function checkWinner() {
+    const lines = [
+        [[0,0],[0,1],[0,2]],
+        [[1,0],[1,1],[1,2]],
+        [[2,0],[2,1],[2,2]],
+        [[0,0],[1,0],[2,0]],
+        [[0,1],[1,1],[2,1]],
+        [[0,2],[1,2],[2,2]],
+        [[0,0],[1,1],[2,2]],
+        [[0,2],[1,1],[2,0]],
+    ];
+
+    for (let line of lines) {
+        const [[x1,y1],[x2,y2],[x3,y3]] = line;
+
+        const v1 = matrix[x1][y1];
+        const v2 = matrix[x2][y2];
+        const v3 = matrix[x3][y3];
+
+        if (v1 !== "" && v1 === v2 && v2 === v3) {
+            alert(v1 + " wins!");
+            return true;
+        }
     }
-    else
-    {
-        matrix[x][y]="O";
-        DisplayMatrix();
+    return false;
+}
+
+
+function TicTacToe(x, y) {
+
+    if (matrix[x][y] !== "") return;
+
+    let moves = CalculateCount();
+    if (moves % 2 === 0) {
+        matrix[x][y] = "O";  
+    } else {
+        matrix[x][y] = "X";
     }
+
+    DisplayMatrix();
+    checkWinner();
 }
