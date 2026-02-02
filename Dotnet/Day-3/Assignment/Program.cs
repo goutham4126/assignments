316dfa8f15dfbb6dc7b385780eaf19c730fb832b
Goutham <goutham4126@gmail.com>
2026-02-02 22:55:20 +0530

    Day-3 daily assignment

---
+++ Dotnet/Day-3/Assignment/Program.cs
@@ -0,0 +1,84 @@
+﻿
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Question-1
+            CallByValueAndReference();
+            // Question-2
+            TryParseExample();
+            // Question-3
+            ArrayExample();
+            // Question-4
+            StringExample();
+            // Question-5
+            ForLoopExample();
+        }
+
+        static void CallByValueAndReference()
+        {
+            int a = 10;
+            int b = 20;
+
+            Console.WriteLine("Before CallByValue: a = " + a);
+            CallByValue(a);
+            Console.WriteLine("After CallByValue: a = " + a);
+
+            Console.WriteLine("Before CallByReference: b = " + b);
+            CallByReference(ref b);
+            Console.WriteLine("After CallByReference: b = " + b);
+        }
+
+        static void CallByValue(int x)
+        {
+            x = 100;
+            Console.WriteLine("Inside CallByValue: x = " + x);
+        }
+
+        static void CallByReference(ref int y)
+        {
+            y = 200;
+            Console.WriteLine("Inside CallByReference: y = " + y);
+        }
+
+        static void TryParseExample()
+        {
+            string input = "123";
+            bool result = int.TryParse(input, out int number);
+
+            if (result)
+                Console.WriteLine("Conversion successful: " + number);
+            else
+                Console.WriteLine("Conversion failed");
+        }
+
+        static void ArrayExample()
+        {
+            int[] numbers = { 10, 20, 30, 40, 50 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+
+        static void StringExample()
+        {
+            string name = "Goutham";
+
+            Console.WriteLine(name.Length);
+            Console.WriteLine(name.ToUpper());
+            Console.WriteLine(name.Substring(0, 4));
+        }
+
+        static void ForLoopExample()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
--- Dotnet/Day-3/practise/Program.cs
+++ Dotnet/Day-3/practise/Program.cs
@@ -1,10 +1,86 @@
-﻿namespace practise
+﻿using System.Text;
+using System.Globalization;
+
+namespace practise
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Math.Abs(-1));
+
+            int[] numbers = new int[10];
+            numbers[0] = 1;
+            numbers[1] = 3;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+
+            // Sort and reverse on array is mutable
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            double amount = 1000.56;
+            Console.WriteLine(amount.ToString("C", new CultureInfo("en-IN")));
+
+
+            Console.WriteLine(Convert.ToInt32(null));
+
+            //Call by value
+            int a = 10;
+            f1(a);
+            Console.WriteLine(a);
+
+            //Call by reference
+            int b = 10;
+            f2(ref b);
+            Console.WriteLine(b);
+
+            //Call by out
+            int c = 10;
+            f3(out c);
+            Console.WriteLine(c);
+
+            //string s = "hello";
+            //s[0] = 'W';
+            //Console.WriteLine(s);
+
+            string name = "Goutham";
+            bool val = int.TryParse(name, out int number);
+            if (val)
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("Invalid number");
+            }
+
         }
+
+        public static void f1(int x)
+        {
+            x = 20;
+            Console.WriteLine(x);
+        }
+
+        public static void f2(ref int x)
+        {
+           
+            Console.WriteLine(x);
+            x = 20;
+        }
+
+        public static void f3(out int x)
+        {
+            x = 20;
+            Console.WriteLine(x);
+        }
+
     }
 }
